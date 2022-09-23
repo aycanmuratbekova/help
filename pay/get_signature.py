773471b1957fb5037ab8aa8c9f648e58cc74f606
aycanmuratbekova <aycanmuratbekova@gmail.com>
2022-09-23 14:01:23 +0600

    New

--- pay/get_signature.py
+++ pay/get_signature.py
@@ -1,8 +1,12 @@
+import crypt
 import hashlib
 from collections import OrderedDict
 
+
 MERCHANT_ID = 535456
 SECRET ="LeFnP16MP6AU6YKc"
+
+
 def get_sig(param):
     """init_payment.php;25;test;{{paybox_merchant_id}};23;molbulak;{{secret_key}}"""
     sig = 'init_payment.php'
@@ -12,3 +16,7 @@ def get_sig(param):
     sig += ';' + SECRET
     hashed_sig = hashlib.md5(sig.encode()).hexdigest()
     return hashed_sig
+
+
+def get_salt():
+    return crypt.mksalt(crypt.METHOD_SHA512)
--- pay/models.py
+++ pay/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+
+
 class Transaction(models.Model):
-    amount = models.IntegerField(default=0)
+    amount = models.DecimalField(default=.0, decimal_places=2, max_digits=9)
     description = models.TextField(verbose_name='description')
-    salt = models.CharField(max_length=25, verbose_name='salt')
+    # salt = models.CharField(max_length=25, verbose_name='salt')
 
